145c4bb00e51d6d1f6ae3fffbf6bd3ddd7ebd3a2
Kai <kkgit@devrim.tech>
2021-03-06 23:56:55 -0800

    Bot Status and Clear Command

--- src/enum/commands.enum.ts
+++ src/enum/commands.enum.ts
@@ -6,5 +6,7 @@ export enum COMMANDS {
 
 export enum ADMIN_COMMANDS {
   podsAdd = "podsAdd <number > 2 >. Generates a random list of words then makes roles based on those random words with the color blue. The number is how many roles you want to make.",
-  podsRemove = "Removes all pods. DANGEROUS! Please do not run unless there is a serious and I mean SERIOUS ISSUE AFFECTING ALL ROLES!!! DO NOT RUN THIS DURING THE HACKATHON OR YOU WILL LOSE ALL YOUR PRIVILEGES!"
+  podsRemove = "Removes all pods. DANGEROUS! Please do not run unless there is a serious and I mean SERIOUS ISSUE AFFECTING ALL ROLES!!! DO NOT RUN THIS DURING THE HACKATHON OR YOU WILL LOSE ALL YOUR PRIVILEGES!",
+  adminHelp = "Admin Only version of Help",
+  clear = "clear < number between 1 and 99 >. Deletes that many messages. "
 }
